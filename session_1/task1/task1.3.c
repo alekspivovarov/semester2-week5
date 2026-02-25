c7ddcdcf5f7065f7e65011c9cb83031c609b2479
alekspivovarov <mkkr0724@leeds.ac.uk>
2026-02-25 17:49:21 +0000

    task1 completed

--- session_1/task1/task1.3.c
+++ session_1/task1/task1.3.c
@@ -7,16 +7,28 @@
    Python program:
    number = int(input("Enter a number: "))
    if (number%3==0) and (number%5==0):
-       print("The number is divisible by 3 and 5")
+       print("The number is divisible by 3 and 5\n")
    else:
-       print("The number is not divisible by both 3 and 5")
+       print("The number is not divisible by both 3 and 5\n")
  */
  
 #include <stdio.h>
 
 int main(void) {
 
+    int number;
+
+    printf("Enter number: ");
+    scanf("%d", &number);
+
+
 	// complete your code here
+    if ( number % 3 == 0 && number % 5 == 0){
+        printf("The number is divisible by 3 and 5");
+    } else {
+        printf("The number is not divisible by both 3 and 5");
+    }
+
 
 	return 0;
 }
--- session_1/task1/task1.4.c
+++ session_1/task1/task1.4.c
@@ -32,6 +32,11 @@ int main(void) {
     scanf("%d", &is_blacklisted);
 	
 	// Complete the rest of the code
+    if ((order_amount > 100 || is_premium == 1) && (!is_blacklisted)){
+        printf("Discount applied");
+    } else {
+        printf("No Discount");
+    }
 
 	return 0;
 }
